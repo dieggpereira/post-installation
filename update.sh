14d3fe27343d0c2e61742a10f133e9932a24bf92
Diego Pereira <dieggpereira@gmail.com>
2021-07-12 15:55:10 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,20 +1,86 @@
 #!/usr/bin/env bash
+echo -e "\e[1;33m  
+>>>>>>>>>>>>>>>>>>>>> ATUALIZANDO O SISTEMA <<<<<<<<<<<<<<<<<<<<<
+\e[0m "
 
 echo -e "\e[1;33m  
-*********** ATUALIZANDO O SISTEMA ***********
+>>>>>>>>>>>>>>>>>>>> Atualizando pacotes APT <<<<<<<<<<<<<<<<<<<<
 \e[0m "
+
 sudo apt-get update
-echo ""
+
 sudo apt-get dist-upgrade -y
-echo ""
+
+if [[ $? -gt 0 ]] 
+then
+echo -e "\e[1;31m  
+>>>>>>>>>>>>>>>>>>>>>>>>>> Apt: Falhou. <<<<<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+else
+rm -rf rm /home/pi/queue/*
+echo -e "\e[1;32m  
+>>>>>>>>>>>>>>>>>>>>>>>>> Apt: Sucesso. <<<<<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+fi
+echo -e "\e[1;33m  
+>>>>>>>>>>>>>>>>>>>>>>> Atualizando Snaps <<<<<<<<<<<<<<<<<<<<<<<
+\e[0m "
 sudo snap refresh
-echo ""
+if [[ $? -gt 0 ]] 
+then
+   echo -e "\e[1;31m  
+>>>>>>>>>>>>>>>>>>>>>>>> Snaps: Falhou. <<<<<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+else
+   rm -rf rm /home/pi/queue/*
+   echo -e "\e[1;32m  
+>>>>>>>>>>>>>>>>>>>>>>>> Snaps: Sucesso. <<<<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+fi
+echo -e "\e[1;33m  
+>>>>>>>>>>>>>>>>>>>>>> Atualizando Flatpaks <<<<<<<<<<<<<<<<<<<<<
+\e[0m "
 flatpak update -y
-echo ""
+if [[ $? -gt 0 ]] 
+then
+   echo -e "\e[1;31m  
+>>>>>>>>>>>>>>>>>>>>>>> Flatpaks: Falhou. <<<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+else
+   rm -rf rm /home/pi/queue/*
+   echo -e "\e[1;32m  
+>>>>>>>>>>>>>>>>>>>>>> Flatpaks: Sucesso. <<<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+fi
+echo -e "\e[1;33m  
+>>>>>>>>>>>>>>>>>>>>>> Limpando o sistema <<<<<<<<<<<<<<<<<<<<<<<
+\e[0m "
 sudo apt-get autoclean
-echo ""
+if [[ $? -gt 0 ]] 
+then
+   echo -e "\e[1;31m  
+>>>>>>>>>>>>>>>>>>>>>> Autoclean: Falhou. <<<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+else
+   rm -rf rm /home/pi/queue/*
+   echo -e "\e[1;32m  
+>>>>>>>>>>>>>>>>>>>>> Autoclean: Sucesso. <<<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+fi
+
 sudo apt-get autoremove -y
+if [[ $? -gt 0 ]] 
+then
+   echo -e "\e[1;31m  
+>>>>>>>>>>>>>>>>>>>>>> Autoremove: Falhou. <<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+else
+   rm -rf rm /home/pi/queue/*
+   echo -e "\e[1;32m  
+>>>>>>>>>>>>>>>>>>>>> Autoremove: Sucesso. <<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+fi
 echo -e "\e[1;32m 
-*********** O COMPUTADOR ESTÁ ATUALIZADO ***********
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Fim <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 \e[0m "
 # ---------------------------------------------------------------------- #
