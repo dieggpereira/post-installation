1589b8c2a43352706b1bd8cb6a6667035a0ddcb4
Diego Pereira <80273980+dieggpereira@users.noreply.github.com>
2021-10-09 18:20:36 -0300

    update

--- update-wsl.sh
+++ update-wsl.sh
@@ -1,12 +1,37 @@
 #!/usr/bin/env bash
+echo -e "\e[1;33m  
+>>>>>>>>>>>>>>>>>>>>> ATUALIZANDO O SISTEMA <<<<<<<<<<<<<<<<<<<<<
+\e[0m "
 
-echo " 
-************* ATUALIZANDO O SISTEMA *************
- "
-sudo apt update
-echo ""
-sudo apt dist-upgrade -y
-echo "
-********** O COMPUTADOR ESTÁ ATUALIZADO **********
- "
+echo -e "\e[1;33m  
+>>>>>>>>>>>>>>>>>>>> Atualizando pacotes APT <<<<<<<<<<<<<<<<<<<<
+\e[0m "
+
+sudo apt-get update
+
+if [[ $? -gt 0 ]] 
+then
+echo -e "\e[1;31m  
+>>>>>>>>>>>>>>>>>>>>> Repositórios: Falhou. <<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+else
+rm -rf rm /home/pi/queue/*
+echo -e "\e[1;32m  
+>>>>>>>>>>>>>>>>>>>>> Repositórios: Sucesso. <<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+fi
+
+sudo apt-get upgrade -y
+
+if [[ $? -gt 0 ]] 
+then
+echo -e "\e[1;31m  
+>>>>>>>>>>>>>>>>>>>>>>>>>> Apt: Falhou. <<<<<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+else
+rm -rf rm /home/pi/queue/*
+echo -e "\e[1;32m  
+>>>>>>>>>>>>>>>>>>>>>>>>> Apt: Sucesso. <<<<<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+fi
 # ---------------------------------------------------------------------- #
