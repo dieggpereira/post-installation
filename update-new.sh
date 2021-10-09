a054387fd42ee4c5d630636791d34729cfdb3e3c
Diego Pereira <80273980+dieggpereira@users.noreply.github.com>
2021-10-09 17:57:37 -0300

    Create update-new.sh

---
+++ update-new.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo " 
+************* ATUALIZANDO O SISTEMA *************
+ "
+sudo apt update
+echo ""
+sudo apt dist-upgrade -y
+echo "
+********** O COMPUTADOR ESTÁ ATUALIZADO **********
+ "
+# ---------------------------------------------------------------------- #
