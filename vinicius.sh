dbd95977a0715474fed36720e57000e7c71567e1
Diego Pereira <80273980+dieggpereira@users.noreply.github.com>
2022-02-22 13:57:25 -0300

    Create vinicius.sh

---
+++ vinicius.sh
@@ -0,0 +1,166 @@
+#!/usr/bin/env bash
+# ------------------------ COMANDO PARA RODAR ------------------------- #
+# ./diego.sh |& tee -a resultados.txt --------------------------------- #
+
+# ----------------------------- VARIÁVEIS ----------------------------- #
+URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+URL_4K_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.19.4-1_amd64.deb"
+mkdir $HOME/temp
+DIRETORIO_DOWNLOADS="$HOME/temp"
+
+LISTA_APT=(
+  cheese
+  flatpak
+  gnome-software-plugin-flatpak
+  gnome-tweaks
+  gparted
+  grsync
+  grub-customizer
+  indicator-sound-switcher
+  inxi
+  lsb
+  lsb-base
+  lsb-core
+  lsb-invalid-mta
+  lsb-printing
+  lsb-release
+  lsb-security
+  mlocate
+  ncdu
+  neofetch
+  onlyoffice-desktopeditors
+  sl
+  synaptic
+  transmission-gtk
+  ubuntu-restricted-extras
+)
+
+LISTA_FLATPAK=(
+  com.discordapp.Discord
+  com.github.jeromerobert.pdfarranger
+  com.obsproject.Studio
+  com.skype.Client
+  com.spotify.Client
+  com.uploadedlobster.peek
+  fr.handbrake.ghb
+  io.typora.Typora
+  org.audacityteam.Audacity
+  org.telegram.desktop
+  org.videolan.VLC 
+  us.zoom.Zoom
+)
+
+# ---------------------------------------------------------------------- #
+
+# ----------------------------- REQUISITOS ----------------------------- #
+
+## Removendo travas eventuais do apt ##
+echo " 
+REMOVENDO TRAVAS DO APT
+ "
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
+
+## Atualizando o repositório ##
+
+echo " 
+ATUALIZANDO OS REPOSITÓRIOS
+ "
+sudo apt update -y
+
+## Atualizando os pacotes ##
+echo " 
+ATUALIZANDO OS PACOTES
+ "
+sudo apt upgrade -y
+sudo apt autoremove -y
+
+## Ajustando tema ##
+echo " 
+AJUSTANDO TEMA
+ "
+gsettings set org.gnome.desktop.interface gtk-theme "Yaru-dark"
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 30
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide 'true'
+gsettings set org.gnome.desktop.interface show-battery-percentage 'true'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 0
+gsettings set org.gnome.desktop.session idle-delay 0
+
+## Adicionando repositórios de terceiros##
+echo " 
+ADICIONANDO REPOSITÓRIOS DE TERCEIROS
+ "
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+echo "deb https://download.onlyoffice.com/repo/debian squeeze main" | sudo tee -a /etc/apt/sources.list
+
+# ---------------------------------------------------------------------- #
+
+# ----------------------------- EXECUÇÃO ----------------------------- #
+## Atualizando o repositório depois da adição de novos repositórios ##
+echo " 
+ATUALIZANDO OS REPOSITÓRIOS
+ "
+sudo apt update -y
+
+# Instalar programas no apt
+echo " 
+INSTALANDO PACOTES APT
+ "
+sudo apt -y install ${LISTA_APT[@]}
+
+## Download e instalaçao de programas externos ##
+echo " 
+DOWNLOAD DE PACOTES EXTERNOS
+ "
+cd "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_4K_DOWNLOADER"       -P "$DIRETORIO_DOWNLOADS"
+
+## Instalando pacotes .deb e .run baixados na sessão anterior ##
+echo " 
+INSTALANDO PACOTES EXTERNOS
+ "
+sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+
+## Corrigindo possíveis erros até aqui ##
+echo " 
+BUSCANDO POR PACOTES AUSENTES
+ "
+sudo apt update -y
+sudo apt install -f -y
+sudo apt upgrade -y
+sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+sudo apt autoremove -y
+rm -rf $DIRETORIO_DOWNLOADS
+
+## Instalando pacotes Flatpak ##
+echo " 
+INSTALANDO PACOTES FLATPAK
+ "
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub ${LISTA_FLATPAK[@]}
+
+## Instalando pacotes Snap ##
+echo " 
+INSTALANDO PACOTES SNAP
+ "
+sudo snap install photogimp visualg prospect-mail && sudo snap install code --classic
+
+# ---------------------------------------------------------------------- #
+
+# ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
+## Finalização, atualização e limpeza##
+echo " 
+FINALIZANDO INSTALAÇÃO E LIMPANDO O SISTEMA
+ "
+sudo apt update && sudo apt dist-upgrade -y
+flatpak update -y
+sudo apt autoclean
+sudo apt autoremove -y
+echo " 
+O COMPUTADOR SERÁ REINICIADO
+ "
+sudo shutdown -r
+# ---------------------------------------------------------------------- #
