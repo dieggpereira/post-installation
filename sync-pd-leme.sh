3fa9e1d870a1121f56a91646bb058d582c0788a0
Diego Pereira <80273980+dieggpereira@users.noreply.github.com>
2022-03-06 12:37:21 -0300

    Create sync-pd-leme.sh

---
+++ sync-pd-leme.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+echo -e "\e[1;33m 
+>>>>>>>>>>>>>>>>>>> Sincronizando Documentos. <<<<<<<<<<<<<<<<<<<
+\e[0m "
+rsync -r -t -v --progress --delete -s -h /home/diego/Documents/Leme /media/diego/"Diego 8 GB"
+if [[ $? -gt 0 ]] 
+then
+   echo -e "\e[1;31m  
+>>>>>>>>>>>>>>>>>>>>>> Documentos: Falhou. <<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+else
+   rm -rf rm /home/pi/queue/*
+   echo -e "\e[1;32m  
+>>>>>>>>>>>>>>>>>>>>> Documentos: Sucesso. <<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+fi
+
+echo -e "\e[1;34m
+Concluído em: $(date +%T). \e[0m"
+echo " "
+fi
