2b18efc95987aa678b5741dbe175b58ff8a70379
Diego Pereira <80273980+dieggpereira@users.noreply.github.com>
2022-03-20 15:01:05 -0300

    Update diego snap list teste.sh

--- diego snap list teste.sh
+++ diego snap list teste.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
-# ------------------------ COMANDO PARA RODAR ------------------------- #
-# sudo ./diego.sh |& tee -a resultados.txt --------------------------------- #
-
-# ----------------------------- VARIÁVEIS ----------------------------- #
-
 LISTA_SNAP=(
-  code --classic
+  #code --classic
   indicator-sound-switcher
   photogimp
   telegram-desktop
   visualg
 )
-
 ## Instalando pacotes Snap ##
 echo " 
 INSTALANDO PACOTES SNAP
  "
 sudo snap install ${LISTA_SNAP[@]}
-
-
 # ---------------------------------------------------------------------- #
