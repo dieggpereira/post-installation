60d094091d6eba66d5a8402842385aacd3d6250d
Diego Pereira <80273980+dieggpereira@users.noreply.github.com>
2022-04-23 18:17:31 -0300

    Update diego.sh

--- diego.sh
+++ diego.sh
@@ -4,10 +4,9 @@
 
 # ----------------------------- VARIÁVEIS ----------------------------- #
 URL_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-URL_EPSON_DRIVER="http://download.ebz.epson.net/dsc/op/stable/debian/dists/lsb3.2/main/binary-amd64/epson-inkjet-printer-201207w_1.0.0-1lsb3.2_amd64.deb"
 URL_FOXIT_READER="https://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.4/en_us/FoxitReader.enu.setup.2.4.4.0911.x64.run.tar.gz"
-URL_GITHUB_DESKTOP="https://github.com/shiftkey/desktop/releases/download/release-2.9.12-linux3/GitHubDesktop-linux-2.9.12-linux3.deb"
-URL_4K_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.20.0-1_amd64.deb"
+URL_GITHUB_DESKTOP="https://github.com/shiftkey/desktop/releases/download/release-2.9.15-linux1/GitHubDesktop-linux-2.9.15-linux1.deb"
+URL_4K_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.20.2-1_amd64.deb"
 mkdir $HOME/temp
 DIRETORIO_DOWNLOADS="$HOME/temp"
 
@@ -123,7 +122,6 @@ DOWNLOAD DE PACOTES EXTERNOS
  "
 cd      "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_CHROME"              -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_EPSON_DRIVER"        -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_FOXIT_READER"        -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_GITHUB_DESKTOP"      -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_4K_DOWNLOADER"       -P "$DIRETORIO_DOWNLOADS"
