443f9475f1fbea95435b7380bc801383a8af8ad0
Diego Pereira <80273980+dieggpereira@users.noreply.github.com>
2022-05-07 16:57:45 -0300

    Update sync-rose-notebook-pd.sh

--- sync-rose-notebook-pd.sh
+++ sync-rose-notebook-pd.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 echo -e "\e[1;33m 
+>>>>>>>>>>>>>>> Sincronizando Área de Trabalho. <<<<<<<<<<<<<<<<
+\e[0m "
+rsync -r -t -v --progress --delete -s -h --exclude=".*/" /home/rosemeire/"Área de Trabalho"/ /media/rosemeire/"Rose 8 GB"/
+if [[ $? -gt 0 ]] 
+then
+   echo -e "\e[1;31m  
+>>>>>>>>>>>>>>>>>> Área de Trabalho: Falhou. <<<<<<<<<<<<<<<<<<<
+\e[0m    "
+else
+   rm -rf rm /home/pi/queue/*
+   echo -e "\e[1;32m  
+>>>>>>>>>>>>>>>>> Área de Trabalho: Sucesso. <<<<<<<<<<<<<<<<<<<
+\e[0m    "
+fi
+
+echo -e "\e[1;33m 
 >>>>>>>>>>>>>>>>>>> Sincronizando Documentos. <<<<<<<<<<<<<<<<<<
 \e[0m "
 rsync -r -t -v --progress --delete -s -h --exclude=".*/" /home/rosemeire/Documentos/ /media/rosemeire/"Rose 8 GB"/
@@ -28,21 +44,5 @@ else
    rm -rf rm /home/pi/queue/*
    echo -e "\e[1;32m  
 >>>>>>>>>>>>>>>>>>>>> Downloads: Sucesso. <<<<<<<<<<<<<<<<<<<<<<
-\e[0m    "
-fi
-
-echo -e "\e[1;33m 
->>>>>>>>>>>>>>> Sincronizando Área de Trabalho. <<<<<<<<<<<<<<<<
-\e[0m "
-rsync -r -t -v --progress --delete -s -h --exclude=".*/" /home/rosemeire/"Área de Trabalho"/ /media/rosemeire/"Rose 8 GB"/
-if [[ $? -gt 0 ]] 
-then
-   echo -e "\e[1;31m  
->>>>>>>>>>>>>>>>>> Área de Trabalho: Falhou. <<<<<<<<<<<<<<<<<<<
-\e[0m    "
-else
-   rm -rf rm /home/pi/queue/*
-   echo -e "\e[1;32m  
->>>>>>>>>>>>>>>>> Área de Trabalho: Sucesso. <<<<<<<<<<<<<<<<<<<
 \e[0m    "
 fi
