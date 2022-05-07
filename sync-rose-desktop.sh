55ad4fe782c11cd7bd8e501abe71d223e9953b3a
Diego Pereira <80273980+dieggpereira@users.noreply.github.com>
2022-05-07 16:13:35 -0300

    new files

---
+++ sync-rose-desktop.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo -e "\e[1;33m 
+>>>>>>>>>>>>>>>>>>> Sincronizando Pasta pessoal. <<<<<<<<<<<<<<<<<<<
+\e[0m "
+rsync -r -t -v --progress --delete -s -h --exclude=".*/" /home/rosemeire/ /media/rosemeire/"Diego 1 TB"/Diego/Backup/Rose/"Ubuntu Desktop"/
+if [[ $? -gt 0 ]] 
+then
+   echo -e "\e[1;31m  
+>>>>>>>>>>>>>>>>>>>>>> Pasta pessoal: Falhou. <<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+else
+   rm -rf rm /home/pi/queue/*
+   echo -e "\e[1;32m  
+>>>>>>>>>>>>>>>>>>>>> Pasta pessoal: Sucesso. <<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+fi
