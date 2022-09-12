9e94e4e6b7a62685821de239ea5411c4f3d34241
Diego Pereira <80273980+dieggpereira@users.noreply.github.com>
2022-09-12 13:54:39 -0300

    Update sync-pd.sh

--- sync-pd.sh
+++ sync-pd.sh
@@ -2,7 +2,7 @@
 echo -e "\e[1;33m 
 >>>>>>>>>>>>>>>>>>> Sincronizando Documentos. <<<<<<<<<<<<<<<<<<<
 \e[0m "
-rsync -r -t -v --progress --delete -s -h /home/diego/Documents /media/diego/"Diego 64 GB"
+rsync -r -t -v --progress --delete --size-only --modify-window=1 -s /home/diego/Documents '/media/diego/Diego 64 GB'
 if [[ $? -gt 0 ]] 
 then
    echo -e "\e[1;31m  
@@ -17,7 +17,7 @@ fi
 echo -e "\e[1;33m 
 >>>>>>>>>>>>>>>>>>>>> Sincronizando Imagens. <<<<<<<<<<<<<<<<<<<<
 \e[0m "
-rsync -r -t -v --progress --delete -s -h /home/diego/Images /media/diego/"Diego 64 GB"
+rsync -r -t -v --progress --delete --size-only --modify-window=1 -s /home/diego/Images /media/diego/"Diego 64 GB"
 if [[ $? -gt 0 ]] 
 then
    echo -e "\e[1;31m  
