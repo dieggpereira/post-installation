783666901e2d3069f3881357249eb46b6e6739a2
Diego Pereira <80273980+dieggpereira@users.noreply.github.com>
2023-01-27 22:40:59 -0300

    sync ssd + renames

---
+++ sync-ssd.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+echo -e "\e[1;33m 
+>>>>>>>>>>>>>>>>>>> Sincronizando Documentos. <<<<<<<<<<<<<<<<<<<
+\e[0m "
+rsync -r -t -v --progress --delete --size-only --modify-window=1 -s /home/diego/Documents /mnt/Arquivos
+if [[ $? -gt 0 ]] 
+then
+   echo -e "\e[1;31m  
+>>>>>>>>>>>>>>>>>>>>>> Documentos: Falhou. <<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+else
+   rm -rf rm /home/pi/queue/*
+   echo -e "\e[1;32m  
+>>>>>>>>>>>>>>>>>>>>> Documentos: Sucesso. <<<<<<<<<<<<<<<<<<<<<<
+\e[0m    "
+fi
+
+echo -e "\e[1;33m 
+>>>>>>>>>>>>>>>>>>> Finalizando transferências. <<<<<<<<<<<<<<<<< \e[0m "
+sync
+echo -e "\e[1;34m 
+Concluído em: $(date +%T). \e[0m"
+echo " "
