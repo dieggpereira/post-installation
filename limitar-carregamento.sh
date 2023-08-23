b45be59340108b826c317f2fcbb5f7fd2ebaa756
Diego Pereira <80273980+dieggpereira@users.noreply.github.com>
2023-08-23 12:48:55 -0300

    Update limitar-carregamento.sh

--- limitar-carregamento.sh
+++ limitar-carregamento.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 atual=$(sudo cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
 if [ $atual -eq '1' ]; then
-    echo "Estava ligado e foi DESLIGADO."
+    echo -e "\e[1;33mEstava ligado e foi DESLIGADO. \e[0m"
     echo 0 | sudo tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode
 else
-    echo "Estava desligado e foi LIGADO."
+    echo -e "\e[1;33mEstava desligado e foi LIGADO. \e[0m"
     echo 1 | sudo tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode
 fi
