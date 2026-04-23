cac45af18b2ca96a9e65d6b56cb3b44a99501436
Diego Pereira <80273980+dieggpereira@users.noreply.github.com>
2026-04-23 12:03:12 -0300

    Update diego.sh

--- diego.sh
+++ diego.sh
@@ -4,13 +4,12 @@
 
 # ----------------------------- VARIÁVEIS ----------------------------- #
 URL_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-URL_FOXIT_READER="https://cdn78.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.4/en_us/FoxitReader.enu.setup.2.4.5.0727.x64.run.tar.gz"
-URL_4K_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloaderplus_1.5.3-1_amd64.deb"
 mkdir $HOME/temp
 DIRETORIO_DOWNLOADS="$HOME/temp"
 
 LISTA_APT=(
-  ## audio-recorder
+  btop
+  curl
   flatpak
   gimp
   git-all
@@ -18,47 +17,35 @@ LISTA_APT=(
   gnome-software-plugin-flatpak
   gnome-tweaks
   gparted
-  grsync
-  ## grub-customizer
+  grub-customizer
   inxi
   menulibre
-  ## mlocate
-  nala
   ncdu
   neofetch
-  notion-app
-  nvtop
-  ## papirus-icon-theme
   sl
   synaptic
-  transmission-gtk
-  ## ubuntu-restricted-extras
+  timeshift
+  transmission
+  ubuntu-restricted-extras
   xarchiver
 )
 
 LISTA_FLATPAK=(
   com.belmoussaoui.Decoder
-  com.bitwarden.desktop
-  com.discordapp.Discord
   com.github.jeromerobert.pdfarranger
-  com.github.johnfactotum.Foliate
   com.github.tchx84.Flatseal
   com.obsproject.Studio
+  com.poweriso.PowerISO
+  com.rafaelmardojai.Blanket
   com.sweethome3d.Sweethome3d
-  com.todoist.Todoist
-  com.spotify.Client
-  com.sweethome3d.Sweethome3d
-  com.todoist.Todoist
-  com.uploadedlobster.peek
   fr.handbrake.ghb
-  fr.romainvigier.MetadataCleaner
-  io.github.nate_xyz.Paleta
   io.github.shiftey.Desktop
+  io.github.tobagin.scramble
   io.gitlab.theevilskeleton.Upscaler
-  io.missioncenter.MissionCenter
   org.audacityteam.Audacity
-  org.libreoffice.LibreOffice
   org.kde.kdenlive
+  org.kde.okular
+  org.telegram.desktop
   org.torproject.torbrowser-launcher
   org.videolan.VLC
   org.zotero.Zotero
@@ -66,12 +53,12 @@ LISTA_FLATPAK=(
 )
 
 LISTA_SNAP=(
-  'asar --classic'
   btop
-  'code --classic'
   fast
   indicator-sound-switcher
-  telegram-desktop
+  rclone
+  spotify
+  steam
   utm-no
 )
 # ---------------------------------------------------------------------- #
@@ -98,16 +85,13 @@ ATUALIZANDO OS PACOTES
  "
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
-sudo apt install curl
 
 ## Adicionando repositórios de terceiros##
 echo " 
 ADICIONANDO REPOSITÓRIOS DE TERCEIROS
 "
-## sudo add-apt-repository ppa:audio-recorder -y
-echo "deb [trusted=yes] https://apt.fury.io/notion-repackaged/ /" | sudo tee /etc/apt/sources.list.d/notion-repackaged.list
-## sudo add-apt-repository ppa:papirus/papirus -y
-## sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
+sudo add-apt-repository ppa:papirus/papirus -y
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
 
 # ---------------------------------------------------------------------- #
 
@@ -130,25 +114,20 @@ DOWNLOAD DE PACOTES EXTERNOS
 "
 cd      "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_CHROME"              -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_FOXIT_READER"        -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_4K_DOWNLOADER"       -P "$DIRETORIO_DOWNLOADS"
-tar -vzxf *.tar.gz
 
 ## Instalando pacotes .deb e .run baixados na sessão anterior ##
 echo " 
 INSTALANDO PACOTES EXTERNOS
 "
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
-chmod +x *.run
-sudo ./*.run
+
 ## Corrigindo possíveis erros até aqui ##
 echo " 
 BUSCANDO POR PACOTES AUSENTES
 "
 sudo apt update -y
 sudo apt install -f -y
-sudo apt upgrade -y
-sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+sudo apt dist-upgrade -y
 sudo apt autoremove -y
 rm -rf $DIRETORIO_DOWNLOADS
 
@@ -165,8 +144,6 @@ INSTALANDO PACOTES SNAP
 "
 sudo snap refresh
 sudo snap install ${LISTA_SNAP[@]}
-sudo snap install code --classic
-sudo snap install asar --classic
 
 # ---------------------------------------------------------------------- #
 
@@ -184,5 +161,5 @@ sudo apt autoremove -y
 echo " 
 O COMPUTADOR SERÁ REINICIADO
 "
-sudo shutdown -r
+shutdown -r now
 # ---------------------------------------------------------------------- #
